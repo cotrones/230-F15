e601896cae06673d946d9c189016cd212fdbbd51
Thomas Cotroneo <tcotroneo@drew.edu>
2015-12-19 15:13:52 -0500

    Array Queue

--- Queue/src/submission/ArrayQueue.java
+++ Queue/src/submission/ArrayQueue.java
@@ -3,37 +3,51 @@ package submission;
 import adt.Queue;
 
 public class ArrayQueue<T> implements Queue<T> {
-	private T[] data;
+	@SuppressWarnings("unchecked")
+	private T[] data = (T[]) new Object[105];
 	private int head, tail;
 	
 	@Override
 	public void enqueue(T newEntry) {
-		// TODO Auto-generated method stub
-		
+		if (data[head] == null)
+			data[head] = newEntry;
+		else
+			tail++;
+			data[tail] = newEntry;
 	}
 
 	@Override
 	public T dequeue() {
-		// TODO Auto-generated method stub
+		if (!isEmpty()) {
+			T temp = data[head];
+			data[head] = null;
+			if (head == data.length-1) {
+				head = 0;
+			}
+			else {
+				head++;
+			}
+			return temp;
+		}
 		return null;
 	}
 
 	@Override
 	public T peek() {
-		// TODO Auto-generated method stub
+		if (!isEmpty()) {
+			return data[0];
+		}
 		return null;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return data[tail] == null;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		data[tail] = null;
 	}
 	
 	public String toString() {
@@ -46,6 +60,11 @@ public class ArrayQueue<T> implements Queue<T> {
 			s += " -> " + data[i];
 		s+= "\n";
 		return s;
+	}
+	
+	public static void main(String args[]) {
+		ArrayQueue<String> aq = new ArrayQueue<String>();
+		aq.enqueue("Thomas");
 	}
 
 }
